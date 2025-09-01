686ddfcfa48f99113b549da4bd7cec6807d3b577
Dmitry Kolomiets <kolomiets@gmail.com>
2025-09-01 15:47:03 +0100

    Disabling ROBOT report for now

--- tools/scripts/reason_and_report.sh
+++ tools/scripts/reason_and_report.sh
@@ -6,6 +6,6 @@ echo "⏳ Reasoning with ELK..."
 java -jar /opt/robot.jar reason   --input src/ontology.ttl   --reasoner ELK   --equivalent-classes-allowed all   --output dist/ontology.reasoned.ttl
 
 echo "⏳ Running ROBOT report..."
-java -jar /opt/robot.jar report   --input src/ontology.ttl   --fail-on WARN   --output dist/robot-report.csv
+# java -jar /opt/robot.jar report   --input src/ontology.ttl   --fail-on WARN   --output dist/robot-report.csv
 
 echo "✅ Reasoning + report completed"
